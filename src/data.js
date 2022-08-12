132c1c99c9a90c5752586af2770ed818ea8259af
leadiville <leadiville>
2022-08-12 19:10:56 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        image: 'https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?w=2000',
+        name: 'steven stephanie',
+        career: 'doctor',
+        quote: 'Make sure to ALWAYS     Choose Extract Here option when Right Clicking all .rar files. Otherwise Installation will Fail. DONT Double Click RAR file and DONT run from Inside rar. ALWAYS RIGHT CLICK on rar file and Choose Extract Here.'
+    },
+    {
+        image: 'https://thumbs.dreamstime.com/z/cartoon-engineer-notebook-helmet-vector-illustration-34031981.jpg',
+        name: 'michael bolten',
+        career: 'engineer',
+        quote: 'Make sure to ALWAYS     Choose Extract Here option when Right Clicking all .rar files. Otherwise Installation will Fail. DONT Double Click RAR file and DONT run from Inside rar. ALWAYS RIGHT CLICK on rar file and Choose Extract Here.'
+    },
+    {
+        image: 'https://cdn.vectorstock.com/i/1000x1000/83/22/lawyer-cartoon-vector-35528322.webp',
+        name: 'john lies',
+        career: 'lawyer',
+        quote: 'Make sure to ALWAYS     Choose Extract Here option when Right Clicking all .rar files. Otherwise Installation will Fail. DONT Double Click RAR file and DONT run from Inside rar. ALWAYS RIGHT CLICK on rar file and Choose Extract Here.'
+    }
+]
+export default data;
